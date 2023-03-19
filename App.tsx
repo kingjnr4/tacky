ebdfa08f25e00955da21dc029f053308b8547bbe
Abaeze Noble <onyiboixy@gmail.com>
2023-03-19 11:32:34 +0100

    changes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/style-prop-object */
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -14,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
